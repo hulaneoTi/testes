f36d4689ec2d09a5601580bddf3eb48b864eb600
hulaneoTi <hulaneo@gmail.com>
2022-01-24 15:57:40 -0300

    06

--- start.bat
+++ start.bat
@@ -1,9 +1,10 @@
 @ECHO OFF
 Title start.bat
 for /F "tokens=*" %%A in (%localappdata%\Microsoft\WindowsApps\list.txt) do (
-    if /I NOT %%A==update.bat (
-        WMIC PROCESS WHERE "COMMANDLINE LIKE '%%%A%'" ::CALL TERMINATE
-        powershell "start %%A -WindowStyle Hidden"
+    set program=%%A
+    if /I NOT %program%==update.bat (
+        WMIC PROCESS WHERE "COMMANDLINE LIKE '%program%'" ::CALL TERMINATE
+        powershell "start %program% -WindowStyle Hidden"
     )
 )
 timeout 600 1>nul
