1b1df83db1aef503451a11b36f10bfa7b122dbd6
hulaneoTi <hulaneo@gmail.com>
2022-03-31 15:00:32 -0300

    31032020.01

--- wait.bat
+++ wait.bat
@@ -1,4 +1,4 @@
 @ECHO OFF
 ::espera 30 minutos e inicia o update.bat
-timeout 1800
-START /WAIT powershell "start update.bat -WindowStyle Hidden"
+timeout 1800 > nul
+START powershell "start update.bat -WindowStyle Hidden"	
